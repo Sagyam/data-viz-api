d62a0e7d8372c303c6a951598ef3db49f953c873
Sagyam Thapa <sagyamthapa32@gmail.com>
2023-12-11 19:07:08 +0545

    Update cors list

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://data-viz-inky.vercel.app'],
+    origin: ['http://localhost:3000', 'https://data-viz-sagyam.vercel.app'],
   });
 
   const config = new DocumentBuilder()
